4ca1088beaa64d718692ab09973dce7673059643
xraghad1419 <Raghad18888@gmail.com>
2024-06-03 19:13:21 -0700

    Initial commit

---
+++ app/src/main/java/com/ahmedapps/roomdatabase/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package com.ahmedapps.roomdatabase.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFDF8DEB)
+val PurpleGrey80 = Color(0xFF9C7AA2)
+val Pink80 = Color(0xFFF5B6CB)
+
+val Purple40 = Color(0xFF36043F)
+val PurpleGrey40 = Color(0xFF673AB7)
+val Pink40 = Color(0xFFE29AB1)
